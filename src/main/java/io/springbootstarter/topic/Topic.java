e3305f6aac67668edc0edde5031cf1a8e9aed16a
prabhav_thali <prabhav_thali@persistent.co.in>
2018-01-31 16:25:51 +0530

    Spring

---
+++ src/main/java/io/springbootstarter/topic/Topic.java
@@ -0,0 +1,23 @@
+package io.springbootstarter.topic;
+
+public class Topic {
+
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Topic() {
+	}
+
+	public Topic(String name) {
+		super();
+		this.name = name;
+	}
+
+}
